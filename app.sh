6ab5f728a33b68ce224a10b8abdcd0c6e395d308
Glitch (deno-page) <none>
2020-06-13 22:06:19 +0000

    update app.sh

--- app.sh
+++ app.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #### vars
 _APP_PATH="/data/app"
-_REPO_NAME="deno-page"
 _REPO_AUTHOR="blubbll"
-_REPO="https://github.com/$_REPO_AUTHOR/$_REPO_NAME.git"
+_REPO_NAME="deno-page"
 _ENTRY_FILE="index"
-####
+#### dynamics
+_REPO="https://github.com/$_REPO_AUTHOR/$_REPO_NAME.git"
 _PATH=$_APP_PATH/$_REPO_NAME
 #### action
 # clear cache
@@ -14,5 +14,7 @@ rm -r $_PATH/*
 git clone $_REPO $_PATH
 # make script runnable...
 mv $_PATH/$_ENTRY_FILE.ts.js $_PATH/$_ENTRY_FILE.ts
+# ...go to path...
+cd $_PATH
 # ...and run it
 deno run --allow-all $_PATH/$_ENTRY_FILE.ts
