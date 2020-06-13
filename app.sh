5d5597192439cdb5dab8574f7fcdb7f3bda7afc1
Glitch (deno-page) <none>
2020-06-13 22:01:03 +0000

    update runscript

--- app.sh
+++ app.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 #### vars
-_PATH="/data/app"
+_APP_PATH="/data/app"
 _REPO_NAME="deno-page"
 _REPO_AUTHOR="blubbll"
 _REPO="https://github.com/$_REPO_AUTHOR/$_REPO_NAME.git"
-_ENTRY_FILE="https://raw.githubusercontent.com/$_REPO_AUTHOR/$_REPO_NAME/glitch/index.ts.js"
+_ENTRY_FILE="index"
 ####
+_PATH=$_APP_PATH/$_REPO_NAME
+#### action
 # clear cache
-rm  $_PATH/*
+rm -r $_PATH/*
 # clone repo
-git clone $_REPO $_PATH/$_REPO_NAME && cp -R $_PATH/$_REPO_NAME/* $_PATH
-# get entry script and run it
-wget -O $_PATH/script.ts $_ENTRY_FILE && deno run --allow-all script.ts
+git clone $_REPO $_PATH
+# make script runnable...
+mv $_PATH/$_ENTRY_FILE.ts.js $_PATH/$_ENTRY_FILE.ts
+# ...and run it
+deno run --allow-all $_PATH/$_ENTRY_FILE.ts
